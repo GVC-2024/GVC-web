7567fe8eb62f11e0e6bf801d1fb6e90ad2904ad2
SSSslyme <eightslyme@gmail.com>
2024-05-05 21:22:06 +0900

    Update App.js

--- gvc/src/App.js
+++ gvc/src/App.js
@@ -1,17 +1,15 @@
+import React from "react"; //기본 리엑트 모듈
+import { Routes, Route, Link } from 'react-router-dom'; //페이지 이동 .
+
 //페이지 로드.
 import HomePage from "./pages/HomePage"; //로그인 페이지
 import LoginPage from "./pages/LoginPage"; //로그인 페이지SignUp
 import SignUp from "./pages/SignUp"; //로그인 페이지SignUp
+import MeetingSummary from './pages/MeetingSummary';
+import MyPage from './pages/MyPage';
+import AISummary from './pages/AISummary';
 //스타일 로드
 import "./App.css";
-//모듈 가져오기
-import React from "react"; //기본 리엑트 모듈
-import { Routes, Route, Link } from 'react-router-dom'; //페이지 이동 .
-//import styled from "styled-components"; //버튼 만들기 위한 모듈
-//import { View, Dimensions, Image } from 'react-native'; //사진 크기 변화시키기 위한 모듈
-//import { useNavigate } from "react-router-dom"; //페이지 이동 시키는 모듈
-
-//함수 정의
 
 function App() {
     return (
@@ -20,8 +18,10 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/loginPage" element={<LoginPage />} />
                 <Route path="/signUp" element={<SignUp />} />
+                <Route path="/MyPage" element={<MyPage />} />
+                <Route path="/meeting-summary/:id" element={<MeetingSummary />} />
+                <Route path="/ai-summary/:id" element={<AISummary />} />
             </Routes>
-
         </div>
     );
 }
