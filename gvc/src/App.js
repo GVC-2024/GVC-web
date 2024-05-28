a5544fafe89c213c174884efba29817b2e19837e
MinsungKim <166286596+MinsungKimm@users.noreply.github.com>
2024-05-28 19:27:05 +0900

    Update App.js
    App.js 파일 수정

--- gvc/src/App.js
+++ gvc/src/App.js
@@ -1,26 +1,35 @@
-import React from "react"; //기본 리엑트 모듈
-import { Routes, Route, Link } from 'react-router-dom'; //페이지 이동 .
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
 
-//페이지 로드.
-import HomePage from "./pages/HomePage"; //로그인 페이지
-import LoginPage from "./pages/LoginPage"; //로그인 페이지SignUp
-import SignUp from "./pages/SignUp"; //로그인 페이지SignUp
+// 페이지와 컴포넌트 로드
+import Header from "./pages/Header";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+import MyConference from "./pages/MyConference";
 import MeetingSummary from './pages/MeetingSummary';
-import MyPage from './pages/MyPage';
 import AISummary from './pages/AISummary';
-//스타일 로드
+import LoginSearchPage from "./pages/LoginSearch/LoginSearchPage";
+import MyPage from "./pages/MyPage";
+import MyPageCorrection from "./pages/MyPageCorrection";
+
+// 스타일 로드
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/loginPage" element={<LoginPage />} />
                 <Route path="/signUp" element={<SignUp />} />
-                <Route path="/MyPage" element={<MyPage />} />
+                <Route path="/myconference" element={<MyConference />} />
                 <Route path="/meeting-summary/:id" element={<MeetingSummary />} />
                 <Route path="/ai-summary/:id" element={<AISummary />} />
+                <Route path="/loginSearchPage" element={<LoginSearchPage/>}/>
+                <Route path="/myPage" element={<MyPage/>}/>
+                <Route path="/myPageCorrection" element={<MyPageCorrection/>}/>
             </Routes>
         </div>
     );
