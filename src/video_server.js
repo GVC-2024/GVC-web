29fecb38a1bf061d0138b3b5357c9f878020c248
SSSslyme <eightslyme@gmail.com>
2024-07-21 22:53:53 +0900

    Update video_server.js

--- src/video_server.js
+++ src/video_server.js
@@ -27,21 +27,55 @@ const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
   //when someone enter
+  socket.on("leaveRoom", (roomName, leaveSocketId) => {
+    leaveRoomServer(roomName, leaveSocketId);
+    socket.to(roomName).emit("leavePeer", leaveSocketId);
+    console.log(`leave ${leaveSocketId}`);
+    socket.to(leaveSocketId).emit("leaveMyRoom");
+  });
+  //console.log("leaveRodddom"); 
   socket.on("requestServerEnterRoom",(roomName, userNickName)=>{ answerEnterRoom(roomName, userNickName,socket);});
   //offer이 들어온 경우 해당 peer에 대해 offer을 신청해준다.
   socket.on("offer", (offer, peerSocketId) => {socket.to(peerSocketId).emit("receiveOffer", offer, socket.id);});
   //offer에 대한 answer이 들어온 경우.
-  socket.on("answerOffer", (answer, peerSocketId) => {socket.to(peerSocketId).emit("receiveAnswerOffer", answer, socket.id);});
+  socket.on("answerOffer", (answer, peerSocketId) => {socket.to(peerSocketId).emit("receiveAnswerOffer", answer, socket.id); console.log("offer");});
   socket.on("ice", (ice, remoteSocketId) => {socket.to(remoteSocketId).emit("ice", ice, socket.id);});
   socket.on("sendMessage", (roomName, nick, message) => {socket.to(roomName).emit("getMessage", nick, message);});
-  socket.on("sendAnouncment",(roomName, nick, message) => {socket.to(roomName).emit("getAnouncment", message);});
-  socket.on("disconnecting", leaveRoom);
+socket.on("sendAnouncment",(roomName, nick, message) => {socket.to(roomName).emit("getAnouncment", message);});
+  
+
 });
 
+function leaveRoomServer(leaveRoomName,leaveSocketId){
+    // 들어가려는 방 찾기
+    let leaveRoom=null;
+    for (let i = 0; i < allRooms.length; ++i) {
+    
+      //여러 화상회의 방 중에 같은 이름이 발견된 경우
+      if (allRooms[i].roomName === leaveRoomName) {
+        let index = allRooms[i].users.findIndex(user => user.socketId === leaveSocketId);
+        if (index !== -1) {
+          console.log(`leave:${allRooms[i].users[index].userNickName}`);
+          allRooms[i].users=allRooms[i].users.splice(index, 1);
+          allRooms[i].participantsNum=allRooms[i].participantsNum-1;
+          if(allRooms[i].participantsNum===0){
+            console.log(`delete:${allRooms[i].roomName}`);
+            deleteRoom(allRooms[i].roomName);
+          }
+        }
+        
+      }
+    }
+}
+
+function deleteRoom(leaveRoomName){
+  let index = allRooms.findIndex(room => room.roomName === leaveRoomName);
+  allRooms=allRooms.splice(index, 1);
+
+}
 const handleListen = () => console.log(`video on http://localhost:${port_video}`);
 httpServer.listen(port_video, handleListen);
 
-function leaveRoom(){console.log("leaveRoom");}
 function answerEnterRoom(roomName, userNickName, socket){
   let targetRoom = null;// 들어오려는 방
   // 들어가려는 방 찾기
@@ -64,6 +98,7 @@ function answerEnterRoom(roomName, userNickName, socket){
 
     }
   }
+  
   // 방이 없는 경우 새로운 방을 만든다. 
   if (targetRoom===null) {
     let newRoom = {
@@ -85,4 +120,3 @@ function answerEnterRoom(roomName, userNickName, socket){
   console.log("enterRoom:accept");
 
 }
-
