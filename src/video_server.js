e8709d331e31d3af98a5a31373ea2ea245c14767
SSSslyme <eightslyme@gmail.com>
2024-07-26 13:37:27 +0900

    Update video_server.js

--- src/video_server.js
+++ src/video_server.js
@@ -2,7 +2,7 @@ import http from "http";
 import SocketIO from "socket.io";
 import express from "express";
 
-const port_video=9000; //port number.
+const port_video=3000; //port number.
 //db
 
 //
@@ -14,6 +14,7 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home_waiting_room"));
 //app.get("/", (_, res) => res.render("home_chat"));
 app.get("/share_note", (req, res) => res.render("share_note"));
+app.get("/a", (req, res) => res.render("share_note"));
 app.get("/home_meeting", (req, res) => res.render("home_meeting"));
 app.get("/*", (_, res) => res.redirect("/"));
 //데이터 선언
@@ -27,28 +28,78 @@ const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
   //when someone enter
-  socket.on("leaveRoom", (roomName, leaveSocketId) => {
+  socket.on("leaveRoom", (roomName, nickName, leaveSocketId) => {
     leaveRoomServer(roomName, leaveSocketId);
     socket.to(roomName).emit("leavePeer", leaveSocketId);
-    console.log(`leave ${leaveSocketId}`);
+    socket.to(roomName).emit("getAnouncment",`${nickName} leave room`);
     socket.to(leaveSocketId).emit("leaveMyRoom");
   });
   //console.log("leaveRodddom"); 
-  socket.on("requestServerEnterRoom",(roomName, userNickName)=>{ answerEnterRoom(roomName, userNickName,socket);});
+  socket.on("requestServerEnterRoom",(roomName, userNickName)=>{answerEnterRoom(roomName, userNickName,socket);});
   //offer이 들어온 경우 해당 peer에 대해 offer을 신청해준다.
   socket.on("offer", (offer, peerSocketId) => {socket.to(peerSocketId).emit("receiveOffer", offer, socket.id);});
   //offer에 대한 answer이 들어온 경우.
   socket.on("answerOffer", (answer, peerSocketId) => {socket.to(peerSocketId).emit("receiveAnswerOffer", answer, socket.id); console.log("offer");});
   socket.on("ice", (ice, remoteSocketId) => {socket.to(remoteSocketId).emit("ice", ice, socket.id);});
-  socket.on("sendMessage", (roomName, nick, message) => {socket.to(roomName).emit("getMessage", nick, message);});
+  socket.on("sendMessage", (roomName, nick, message) => {recordLogChat(roomName,nick, message); socket.to(roomName).emit("getMessage", nick, message);});
 socket.on("sendAnouncment",(roomName, nick, message) => {socket.to(roomName).emit("getAnouncment", message);});
   
 
 });
 
+function recordLogChat(chatRoomName, chatUser,chatContent){
+  let chatRoomIndex=null;
+  console.log("record chatContent");
+  // 대화하는 방 찾기
+  for (let i = 0; i < allRooms.length; ++i) {
+    if (allRooms[i].roomName === chatRoomName) {
+      chatRoomIndex = i;
+    }
+  }
+  if(chatRoomIndex===null){
+  }else{
+    allRooms[chatRoomIndex].chatLog.push({chatLogUser:chatUser, chatLogContent:chatContent });
+  }
+  //console.log(`${chatRoomIndex} want record chatContent: ${chatContent}`);
+  let chat_length=allRooms[chatRoomIndex].chatLog.length;
+  //console.log(`chat_length: ${chat_length}`);
+  /*for(let i=0;chat_length>i;i++){
+    console.log(`${i} chatLogUser: ${allRooms[chatRoomIndex].chatLog[i].chatLogUser}, `, allRooms[chatRoomIndex].chatLog[i].chatLogContent);
+  }*/
+  
+  
+}
+
+function getLogChat(chatRoomName){
+  let chatRoomIndex=null;
+  let chatContent=null;
+  // 대화하는 방 찾기
+  for (let i = 0; i < allRooms.length; ++i) {
+    if (allRooms[i].roomName === chatRoomName) {
+      chatRoomIndex = i;
+      break;
+    }
+  }
+  if(chatRoomIndex!==null){
+    if(allRooms[chatRoomIndex].chatLog.length>0){
+      chatContent=allRooms[chatRoomIndex].chatLog;
+    }
+    
+  }
+  console.log(`chatContent: ${chatContent}`);
+  if(chatContent!==null){
+    for(let i=0;chatContent.length>i;i++){
+      console.log(`${i} chatLogUser: ${chatContent[i].chatLogUser}, chatLog: ${chatContent[i].chatLogContent}`);
+    }
+    return chatContent;
+  }
+
+  
+  
+}
+
 function leaveRoomServer(leaveRoomName,leaveSocketId){
     // 들어가려는 방 찾기
-    let leaveRoom=null;
     for (let i = 0; i < allRooms.length; ++i) {
     
       //여러 화상회의 방 중에 같은 이름이 발견된 경우
@@ -63,7 +114,7 @@ function leaveRoomServer(leaveRoomName,leaveSocketId){
             deleteRoom(allRooms[i].roomName);
           }
         }
-        
+        break; 
       }
     }
 }
@@ -98,25 +149,31 @@ function answerEnterRoom(roomName, userNickName, socket){
 
     }
   }
-  
-  // 방이 없는 경우 새로운 방을 만든다. 
+  let chatLog;
+  // 방이 없는 경우 새로운 방을 만든다.
   if (targetRoom===null) {
     let newRoom = {
       roomName,
       participantsNum: 0,
-      users: []
+      users: [],
+      chatLog:[]
     };
     console.log("newRoom:create");
     allRooms.push(newRoom);
     targetRoom=newRoom;
+  }else{
+    chatLog=getLogChat(roomName);
   }
 
   //방 정보 갱신
   ++targetRoom.participantsNum;//인원이 추가되었으므로 갱신한다. 
-  targetRoom.users.push({socketId: socket.id, userNickName});
+  targetRoom.users.push({socketId: socket.id, userNickName: userNickName});
   //방에 들어가기
   socket.join(roomName);
+  socket.to(roomName).emit("getAnouncment",`${userNickName} enter room`);
+  socket.emit("getAnouncment",`${userNickName} you enter room`);
   socket.emit("enterRoom",targetRoom.users);
-  console.log("enterRoom:accept");
+  socket.emit("enterRoomChat",targetRoom.chatLog);
+  console.log("enterRoom:accept",targetRoom.users.userNickName);
 
 }
